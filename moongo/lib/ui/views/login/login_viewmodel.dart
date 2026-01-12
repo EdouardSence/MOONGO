d840b1ab9a1f9bb8fe61552e5098d19b90bcc7db
EdouardSence <edouard.sence@ynov.com>
2026-01-12 14:04:05 +0100

    feat(login): ajout de la création de compte avec email et mot de passe

--- moongo/lib/ui/views/login/login_viewmodel.dart
+++ moongo/lib/ui/views/login/login_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:moongo/app/app.locator.dart';
 import 'package:moongo/app/app.router.dart';
 import 'package:stacked/stacked.dart';
@@ -50,12 +51,29 @@ class LoginViewModel extends BaseViewModel {
     notifyListeners();
 
     try {
+      if (!loginMode) {
+        // Création de compte
+        final credential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailAddress,
+          password: password,
+        );
+        // Compte créé avec succès
+        if (kDebugMode) {
+          debugPrint('Compte créé: ${credential.user?.email}');
+        }
+        _navigationService.replaceWithHomeView();
+        return;
+      }
       final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: emailAddress,
         password: password,
       );
-      // Login réussi !
-      print('Utilisateur connecté: ${credential.user?.email}');
+      // Login réussi
+      if (kDebugMode) {
+        debugPrint('Connecté en tant que: ${credential.user?.email}');
+      }
+      // Navigation vers Home
       _navigationService.replaceWithHomeView();
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
