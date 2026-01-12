1480ce082cb3abee371f34aaf189836f33aaaf5b
EdouardSence <edouard.sence@ynov.com>
2026-01-12 12:08:15 +0100

    feat(firebase): ajout de la configuration Firebase et des règles de sécurité

--- moongo/lib/main.dart
+++ moongo/lib/main.dart
@@ -4,9 +4,14 @@ import 'package:moongo/app/app.dialogs.dart';
 import 'package:moongo/app/app.locator.dart';
 import 'package:moongo/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
